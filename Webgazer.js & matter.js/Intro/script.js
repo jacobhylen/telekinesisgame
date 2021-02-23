8d1741db6b10ce5f5590725bc2306f5ddcac80a0
JFranzn <johfranz@hotmail.com>
2021-02-23 14:08:38 +0100

    Created Intro branch
    Creating reference points at specific positions that the user should calibrate at.

--- Webgazer.js & matter.js/Intro/script.js
+++ Webgazer.js & matter.js/Intro/script.js
@@ -55,11 +55,17 @@ var collisionEyeListener = async function (data, clock) {
 var Engine = Matter.Engine,
   Render = Matter.Render,
   World = Matter.World,
-  Bodies = Matter.Bodies;
+  Bodies = Matter.Bodies,
+  Body = Matter.Body,
+  MouseConstraint = Matter.MouseConstraint,
+  Mouse = Matter.Mouse,
+  Events = Matter.Events;
 
 // create an engine
 var engine = Engine.create();
-engine.world.gravity.y = 0.5;
+world = engine.world;
+engine.world.gravity.y = 0;
+engine.world.gravity.x = 0;
 
 // create a renderer
 var render = Render.create({
@@ -73,51 +79,27 @@ var render = Render.create({
   },
 });
 
-// The static boxes
-var boxA = Bodies.rectangle(window.innerWidth / 2 - 200, window.innerHeight / 2, 80, 80, {isStatic: true, render: {
-  fillStyle: 'black',
-  strokeStyle: '#ff6f3c',
+ // add mouse control
+ var mouse = Mouse.create(render.canvas),
+ mouseConstraint = MouseConstraint.create(engine, {
+     mouse: mouse,
+     constraint: {
+         stiffness: 0.2,
+         render: {
+             visible: false
+         }
+     }
+ });
+
+World.add(world, mouseConstraint);
+
+let ball = Matter.Bodies.circle(innerWidth / 2, innerHeight / 2,20, {isStatic: false, render: {
+  fillStyle: '#e84545',
+  strokeStyle: '#2b2e4a',
   lineWidth: 8
 }});
-var boxB = Bodies.rectangle(window.innerWidth / 2 + 200, window.innerHeight / 2, 80, 80, {isStatic: true, render: {
-  fillStyle: 'black',
-  strokeStyle: '#005691',
-  lineWidth: 8
-}});
 
-// create two boxes and a ground
-var boxC = Bodies.rectangle(1200, 50, 80, 80, { render: {
-  // orange
-  fillStyle: '#ff6f3c',
-}});
-var boxD = Bodies.rectangle(200, 50, 80, 80, { render: {
-  //blue
-  fillStyle: '#005691',
-}});
-var boxE = Bodies.rectangle(1200, 50, 80, 80, { render: {
-  // orange
-  fillStyle: '#ff6f3c',
-}});
-var boxF = Bodies.rectangle(200, 50, 80, 80, { render: {
-  //blue
-  fillStyle: '#005691',
-}});
-var boxG = Bodies.rectangle(1200, 50, 80, 80, { render: {
-  // orange
-  fillStyle: '#ff6f3c',
-}});
-var boxH = Bodies.rectangle(200, 50, 80, 80, { render: {
-  // blue
-  fillStyle: '#005691',
-}});
-
-
-boxC.mass = 500;
-boxD.mass = 500;
-boxE.mass = 500;
-boxF.mass = 500;
-boxG.mass = 500;
-boxH.mass = 500;
+//Ground and walls
 
 var ground = Bodies.rectangle(
   window.innerWidth / 2,
@@ -148,10 +130,57 @@ var wall2 = Bodies.rectangle(
   { isStatic: true }
 );
 
+let calibration = 0;
+
+Events.on(mouseConstraint, 'mouseup', function(event) {
+  calibration++;
+
+  var mousePosition = event.mouse.position;
+
+  if (calibration == 1) {
+    Matter.Body.setPosition(ball, { x: 300, y: 300});
+  } else if (calibration == 2) {
+    Matter.Body.setPosition(ball, { x: 300, y: 200});
+  } else if (calibration == 3) {
+    Matter.Body.setPosition(ball, { x: 100, y: 400});
+  } else if (calibration == 4) {
+    Matter.Body.setPosition(ball, { x: 700, y: 500});
+  } else if (calibration == 5) {
+    Matter.Body.setPosition(ball, { x: 600, y: 100});
+  } else if (calibration == 6) {
+    Matter.Body.setPosition(ball, { x: 100, y: 600});
+  }
+
+  console.log(calibration);
+  console.log('mouseup at ' + mousePosition.x + ' ' + mousePosition.y);
+});
+
+Events.on(engine, 'beforeUpdate', function(event) {
+  // make bodyA move up and down
+  // body is static so must manually update velocity for friction to work
+  // var py = 300 + 100 * Math.sin(engine.timing.timestamp * 0.002);
+  // Body.setVelocity(ball, { x: ball.position.x, y: py - ball.position.y });
+  // Body.setPosition(ball, { x: ball.position.x, y: py });
+
+  // if (calibration == 1) {
+  //   Matter.Body.setVelocity(ball, { x: 300, y: 300});
+  // } else if (calibration == 2) {
+  //   Matter.Body.setPosition(ball, { x: 300, y: 200});
+  // } else if (calibration == 3) {
+  //   Matter.Body.setPosition(ball, { x: 100, y: 400});
+  // } else if (calibration == 4) {
+  //   Matter.Body.setPosition(ball, { x: 300, y: 500});
+  // } else if (calibration == 5) {
+  //   Matter.Body.setPosition(ball, { x: 600, y: 100});
+  // } else if (calibration == 6) {
+  //   Matter.Body.setPosition(ball, { x: 100, y: 600});
+  // }
+});
+
 
 
 // add all of the bodies to the world
-World.add(engine.world, [boxA, boxB, boxC, boxD, boxE, boxF, boxG, boxH, ground, ground2, wall1, wall2]);
+World.add(world, [ball, ground, ground2, wall1, wall2, mouseConstraint]);
 
 // run the engine
 Engine.run(engine);
@@ -160,66 +189,8 @@ Engine.run(engine);
 Render.run(render);
 
 setInterval(function () {
-  let collisionAC = Matter.SAT.collides(boxA, boxC);
-  let collisionAE = Matter.SAT.collides(boxA, boxE);
-  let collisionAG = Matter.SAT.collides(boxA, boxG);
-
-  let collisionBD = Matter.SAT.collides(boxB, boxD);
-  let collisionBF = Matter.SAT.collides(boxB, boxF);
-  let collisionBH = Matter.SAT.collides(boxB, boxH);
-
-  if (collisionAC.collided) {
-    boxC.position.x = 2000;
-  }
-  if (collisionAE.collided) {
-    boxE.position.x = 2000;
-  }
-  if (collisionAG.collided) {
-    boxG.position.x = 2000;
-  }
-
-
-  if (collisionBD.collided) {
-    boxD.position.x = 2000;
-  }
-  if (collisionBF.collided) {
-    boxF.position.x = 2000;
-  }
-  if (collisionBH.collided) {
-    boxH.position.x = 2000;
-  }
-  
-/*     if(eyeY > boxA.position.y){
-      boxA.force.y = calculateGravityForce(boxA);
-    }
-    if(eyeY < boxA.position.y){
-      boxA.force.y = -calculateGravityForce(boxA);
-    }
-    console.log(calculateGravityForce(boxA));
-   */
-  boxC.force.x = gravityX(boxC);
-  boxC.force.y = gravityY(boxC);
-
-  boxD.force.x = gravityX(boxD);
-  boxD.force.y = gravityY(boxD);
-
-
-  boxE.force.x = gravityX(boxE);
-  boxE.force.y = gravityY(boxE);
-
-  boxF.force.x = gravityX(boxF);
-  boxF.force.y = gravityY(boxF);
-
-  boxG.force.x = gravityX(boxG);
-  boxG.force.y = gravityY(boxG);
-
-  boxH.force.x = gravityX(boxH);
-  boxH.force.y = gravityY(boxH);
-
   
 }, 1);
-
-
 
 function getDistanceToSingularity(object) {
   let distance = Math.sqrt(
