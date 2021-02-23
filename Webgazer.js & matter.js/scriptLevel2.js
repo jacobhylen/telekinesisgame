89f3f20af9af2e1eab535a7bcccabfd9160c1710
Jacob Hylén <jacob.hylen@gmail.com>
2021-02-23 14:17:24 +0100

    Stuff

--- Webgazer.js & matter.js/scriptLevel2.js
+++ Webgazer.js & matter.js/scriptLevel2.js
@@ -3,6 +3,17 @@ window.saveDataAcrossSessions = true;
 let eyeX = 0;
 let eyeY = 0;
 
+let calibrationCounter = 0;
+let body = document.getElementById("body");
+let calibrationMeter = document.getElementById("calibratingProgress");
+
+body.addEventListener("click", function(){
+  calibrationCounter += 3;
+  
+ 
+  calibrationMeter.style.width= calibrationCounter +"px";
+})
+
 let singularityMass = 1 * Math.pow(10, 12);
 
 window.onload = async function () {
@@ -86,38 +97,7 @@ var boxB = Bodies.rectangle(window.innerWidth / 2 + 200, window.innerHeight / 2,
 }});
 
 // create two boxes and a ground
-var boxC = Bodies.rectangle(1200, 50, 80, 80, { render: {
-  // orange
-  fillStyle: '#ff6f3c',
-}});
-var boxD = Bodies.rectangle(200, 50, 80, 80, { render: {
-  //blue
-  fillStyle: '#005691',
-}});
-var boxE = Bodies.rectangle(1200, 50, 80, 80, { render: {
-  // orange
-  fillStyle: '#ff6f3c',
-}});
-var boxF = Bodies.rectangle(200, 50, 80, 80, { render: {
-  //blue
-  fillStyle: '#005691',
-}});
-var boxG = Bodies.rectangle(1200, 50, 80, 80, { render: {
-  // orange
-  fillStyle: '#ff6f3c',
-}});
-var boxH = Bodies.rectangle(200, 50, 80, 80, { render: {
-  // blue
-  fillStyle: '#005691',
-}});
 
-
-boxC.mass = 500;
-boxD.mass = 500;
-boxE.mass = 500;
-boxF.mass = 500;
-boxG.mass = 500;
-boxH.mass = 500;
 
 var ground = Bodies.rectangle(
   window.innerWidth / 2,
@@ -151,7 +131,7 @@ var wall2 = Bodies.rectangle(
 
 
 // add all of the bodies to the world
-World.add(engine.world, [boxA, boxB, boxC, boxD, boxE, boxF, boxG, boxH, ground, ground2, wall1, wall2]);
+World.add(engine.world, [ground, ground2, wall1, wall2]);
 
 // run the engine
 Engine.run(engine);
@@ -160,63 +140,7 @@ Engine.run(engine);
 Render.run(render);
 
 setInterval(function () {
-  let collisionAC = Matter.SAT.collides(boxA, boxC);
-  let collisionAE = Matter.SAT.collides(boxA, boxE);
-  let collisionAG = Matter.SAT.collides(boxA, boxG);
 
-  let collisionBD = Matter.SAT.collides(boxB, boxD);
-  let collisionBF = Matter.SAT.collides(boxB, boxF);
-  let collisionBH = Matter.SAT.collides(boxB, boxH);
-
-  if (collisionAC.collided) {
-    boxC.position.x = 2000;
-  }
-  if (collisionAE.collided) {
-    boxE.position.x = 2000;
-  }
-  if (collisionAG.collided) {
-    boxG.position.x = 2000;
-  }
-
-
-  if (collisionBD.collided) {
-    boxD.position.x = 2000;
-  }
-  if (collisionBF.collided) {
-    boxF.position.x = 2000;
-  }
-  if (collisionBH.collided) {
-    boxH.position.x = 2000;
-  }
-  
-/*     if(eyeY > boxA.position.y){
-      boxA.force.y = calculateGravityForce(boxA);
-    }
-    if(eyeY < boxA.position.y){
-      boxA.force.y = -calculateGravityForce(boxA);
-    }
-    console.log(calculateGravityForce(boxA));
-   */
-  boxC.force.x = gravityX(boxF);
-  boxC.force.y = gravityY(boxF);
-
-  boxD.force.x = gravityX(boxD);
-  boxD.force.y = gravityY(boxD);
-
-
-  boxE.force.x = gravityX(boxE);
-  boxE.force.y = gravityY(boxE);
-
-  boxF.force.x = gravityX(boxF);
-  boxF.force.y = gravityY(boxF);
-
-  boxG.force.x = gravityX(boxG);
-  boxG.force.y = gravityY(boxG);
-
-  boxH.force.x = gravityX(boxH);
-  boxH.force.y = gravityY(boxH);
-
-  
 }, 1);
 
 
